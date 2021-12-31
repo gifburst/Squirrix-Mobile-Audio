334fc88f88206fb3d07861c3538af6465ab10a08
squirrelcom <84091436+squirrelcom@users.noreply.github.com>
2021-12-31 21:57:17 +0000

    Update date.py

--- bin/date.py
+++ bin/date.py
@@ -1,8 +1,18 @@
 # date command prints date
 
 import time
+import pyttsx3
+
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
 
 
 def main():
     time_stamp = time.strftime('%A, %d %B %Y')
-    print(time_stamp)
+    talk(time_stamp)
