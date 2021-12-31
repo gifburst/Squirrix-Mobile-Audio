5097e92c0c29cdd105d4ef1716edfa9e7d3d189f
squirrelcom <84091436+squirrelcom@users.noreply.github.com>
2021-12-31 22:26:04 +0000

    Update time.py

--- bin/time.py
+++ bin/time.py
@@ -1,7 +1,17 @@
 # time command prints current time
 import time
+import pyttsx3
+
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
 
 
 def main():
     time_stamp = time.strftime('%I:%M:%S %p')
-    print(time_stamp)
+    talk(time_stamp)
