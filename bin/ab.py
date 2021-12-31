447be67c210839974e96b1c22ea46217034f7a90
squirrelcom <84091436+squirrelcom@users.noreply.github.com>
2021-12-31 21:49:00 +0000

    Update ab.py

--- bin/ab.py
+++ bin/ab.py
@@ -1,6 +1,16 @@
+import pyttsx3
+
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
 def main():
-    print("")
-    print("Squirrix Mobile Version 0.1")
-    print("Copyright (c) 2021 Squirrel Computers")
-    print("")
-    print("")
+    talk("")
+    talk("Squirrix Mobile Version 0.1")
+    talk("Copyright (c) 2021 Squirrel Computers")
+    talk("")
+    talk("")
